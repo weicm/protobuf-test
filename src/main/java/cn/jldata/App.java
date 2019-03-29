7718045408852b20b53b0ac84f93346c54f97266
魏常明 <weichangming@julong.cc>
2019-03-29 17:52:03 +0800

    init

---
+++ src/main/java/cn/jldata/App.java
@@ -0,0 +1,13 @@
+package cn.jldata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
